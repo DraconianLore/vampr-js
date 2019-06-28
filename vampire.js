27b44b81aee818abcd532d191fd21a05ca1c3644
Steven Wing <steven.wing82@gmail.com>
2019-06-28 01:34:37 +0000

    complete

--- vampire.js
+++ vampire.js
@@ -10,22 +10,33 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let currentVampire = this;
+    let numberToOrigin = 0;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberToOrigin++;
+    }
+    return numberToOrigin;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /** Stretch **/
@@ -36,9 +47,32 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    // check ranks
+    let thisVampire = this;
+    let thatVampire = vampire;
+    let thisVampireRank = thisVampire.numberOfVampiresFromOriginal;
+    let thatVampireRank = thatVampire.numberOfVampiresFromOriginal;
+    while (thisVampireRank !== thatVampireRank) {
+      if (thisVampireRank > thatVampireRank) {
+        thisVampire = thisVampire.creator;
+        thisVampireRank = thisVampire.numberOfVampiresFromOriginal;
+      } else if (thisVampireRank < thatVampireRank) {
+        thatVampire = thatVampire.creator;
+        thatVampireRank = thatVampire.numberOfVampiresFromOriginal;
+      }
+    }
+    // once ranks are the same find a common ancestor
+    while (thisVampire !== thatVampire) {
+      thisVampire = thisVampire.creator;
+      thatVampire = thatVampire.creator;
+    }
+    return thisVampire;
 
   }
 }
+
+
+
 
 module.exports = Vampire;
 
